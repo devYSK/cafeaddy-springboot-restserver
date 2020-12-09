584ec546a8ca3049fcd787ad5a0b9f47452980f1
Young Soo Kim <41531594+devYSK@users.noreply.github.com>
2020-12-09 17:45:06 +0900

    build.gradle jwt관련 라이브러리 추가

--- src/main/java/xyz/cafeaddy/rest/lcs/web/controller/OwnerController.java
+++ src/main/java/xyz/cafeaddy/rest/lcs/web/controller/OwnerController.java
@@ -19,7 +19,7 @@ public class OwnerController {
     public ResponseEntity<?> duplicateEmailCheck(String email) {
 
         //TODO: url 리소스 get 으로 변경해야함(restful 하지 않음)
-        //TODO:
+
         return ownerService.duplicateEmailCheck(email);
     }
 
